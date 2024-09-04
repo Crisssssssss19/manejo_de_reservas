e49e1053abe15840460f7c0629e73fc6e9e783dd
Cristian Peña <113263798+Crisssssssss19@users.noreply.github.com>
2024-09-04 08:42:51 -0500

    Eliminar Escala

--- src/main/java/com/cris/manejo_de_reservas/repositories/EscalaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cris.manejo_de_reservas.repositories;
-
-import com.cris.manejo_de_reservas.entities.Escala;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EscalaRepository extends JpaRepository<Escala, Long> {
-}
