c0b446c4b5c1730f231acbc919e7a0f8fd6e332b
Cristian Peña <cristiandpm.2004@gmail.com>
2024-09-24 21:16:42 -0500

    Creacion de getAllAerolinea

--- src/main/java/com/cris/manejo_de_reservas/api/AerolineaController.java
+++ src/main/java/com/cris/manejo_de_reservas/api/AerolineaController.java
@@ -1,8 +1,13 @@
 package com.cris.manejo_de_reservas.api;
 
+import com.cris.manejo_de_reservas.entities.Aerolinea;
 import com.cris.manejo_de_reservas.services.aerolinea.AerolineaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/0.1/aerolineas")
@@ -14,5 +19,8 @@ public class AerolineaController {
         this.aerolineaService = aerolineaService;
     }
 
-    
+    @GetMapping()
+    public ResponseEntity<List<Aerolinea>> getAllAerolinea(){
+        return ResponseEntity.ok(aerolineaService.buscarAerolinea());
+    }
 }
